29e27577a5938b41dfd2a37484a34d56bd110e18
cnatasya17 <132834056+cnatasya17@users.noreply.github.com>
2023-05-08 09:55:11 +0700

    Update Crud.php

--- Classes/Crud/Crud.php
+++ Classes/Crud/Crud.php
@@ -1,1 +1,25 @@
+<?php
+    require_once '../../Connect/connection.php';
 
+    class Crud { 
+        private $name;
+
+        public function __construct($table)
+        {
+            $this->name = $table;
+        }
+
+        public function show()
+        {
+            global $connect;
+            $sql = "SELECT * FROM " . $this->name;
+            $execute_query = mysqli_query($connect, $sql);
+            return $execute_query;
+        }
+
+        public function className()
+        {
+            return get_class($this);
+        }
+    }
+?>
